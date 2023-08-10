379cf03482fdb33b68f77dbacfe3ef19b9d8d218
PoliJun <rivenloving@gmail.com>
2023-08-10 08:52:46 +0800

    created dev branch

--- library_system/src/App.java
+++ library_system/src/App.java
@@ -1,5 +1,45 @@
+import utils.KeyboardUtil;
+import view.BooksView;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        if (users != null) {
+            // 登录成功调用主界面
+            BooksView.mainView();
+            // 接收选择的数值
+            int a = KeyboardUtil.readInt();
+            switch (a) {
+                case 1:
+                    System.out.println("查询");
+                    this.queryAllNews();
+                    break;
+                case 2:
+                    System.out.println("新增");
+                    this.addNews();
+                    break;
+                case 3:
+                    System.out.println("修改");
+                    this.queryAllNews();
+
+                    break;
+                case 4:
+                    System.out.println("删除");
+                    this.queryAllNews();
+
+                    break;
+                case 0:
+                    System.out.println("退出......");
+                    // 系统正常退出
+                    System.exit(0);
+                    break;
+
+                default:
+                    System.out.println("没有此选项，请重新输入：");
+                    break;
+            }
+
+        } else {
+            System.out.println("登录失败");
+        }
     }
 }
--- library_system/src/main/LibraryMain.java
+++ library_system/src/main/LibraryMain.java
@@ -17,59 +17,6 @@ import view.BooksView;
 public class LibraryMain {
     private BooksService bs = null;
 
-    public void start() {
-        System.out.println("欢迎来到图书信息管理系统");
-        System.out.println("请输入您的用户名");
-        String loginName = KeyboardUtil.readString();
-        System.out.println("Input your password: ");
-        String password = KeyboardUtil.readString();
-        System.out.println(loginName + password);
-
-        Admin user = new Admin(loginName, password);
-
-        Admin users = new User();
-        users = bs.login(user);
-        if (users != null) {
-            // 登录成功调用主界面
-            BooksView.mainView();
-            // 接收选择的数值
-            int a = KeyboardUtil.readInt();
-            switch (a) {
-                case 1:
-                    System.out.println("查询");
-                    this.queryBooks();
-                    break;
-                case 2:
-                    System.out.println("新增");
-                    this.addBooks();
-                    break;
-                case 3:
-                    System.out.println("修改");
-                    this.updateBooks();
-
-                    break;
-                case 4:
-                    System.out.println("删除");
-                    this.deleteBooks();
-
-                    break;
-                case 0:
-                    System.out.println("退出......");
-                    // 系统正常退出
-                    System.exit(0);
-                    break;
-
-                default:
-                    System.out.println("没有此选项，请重新输入：");
-                    break;
-            }
-
-        } else {
-            System.out.println("登录失败");
-        }
-
-    }
-
     /**
      * @author weihuanchun
      * @return
