e09e57388fe25aa133fa889b3dbca6608c08df68
PoliJun <rivenloving@gmail.com>
2023-08-11 09:07:17 +0800

    while app.java

--- library_system/src/App.java
+++ library_system/src/App.java
@@ -1,4 +1,4 @@
-import java.util.ArrayList;
+
 import java.util.List;
 
 import main.AdminMain;
@@ -29,67 +29,71 @@ public class App {
                 break;
             // i--;
         }
-        // if (i < 1) System.exit(0); // 5 次机会 退出
-        StartView.mainView();
+        // if (i < 1) System.exit(0); // 5 次机会
 
-        int choose = KeyboardUtil.readInt();
-        switch (choose) {
+        while (true) {
+            StartView.mainView();
 
-            case 1:
-                bm = new BooksMain();
-                System.out.println("1.图书查询\t2.图书修改");
-                int choice = KeyboardUtil.readInt();
-                switch (choice) {
-                    case 1:
-                        List<Books> books = bm.queryBooks();
+            int choose = KeyboardUtil.readInt();
+            switch (choose) {
 
-                        if (!books.isEmpty()) {
-                            for (Books book : books) {
-                                System.out.println("");
-                                System.out.println(book.getId() + book.getIsbn() + book.getTitle() + book.getAuthor() +
-                                        book.getPublisher() + book.getPublicationDate() + book.getStatus());
+                case 1:
+                    bm = new BooksMain();
+                    System.out.println("1.图书查询\t2.图书修改");
+                    int choice = KeyboardUtil.readInt();
+                    switch (choice) {
+                        case 1:
+                            List<Books> books = bm.queryBooks();
+
+                            if (!books.isEmpty()) {
+                                for (Books book : books) {
+                                    System.out.println("");
+                                    System.out.println(
+                                            book.getId() + book.getIsbn() + book.getTitle() + book.getAuthor() +
+                                                    book.getPublisher() + book.getPublicationDate() + book.getStatus());
+                                }
+                            } else {
+                                System.out.println("没有找到书籍");
                             }
-                        } else {
-                            System.out.println("没有找到书籍");
-                        }
-                        break;
-                    case 2:
-                        Books book = new Books();
-                        int res = bm.modifyBooks(book);
-                        if (res != 0) {
-                            System.out.println("修改成功！");
-                        } else {
-                            System.out.println("修改失败");
-                        }
-                        break;
+                            break;
+                        case 2:
+                            Books book = new Books();
+                            int res = bm.modifyBooks(book);
+                            if (res != 0) {
+                                System.out.println("修改成功！");
+                            } else {
+                                System.out.println("修改失败");
+                            }
+                            break;
 
-                }
-                break;
-            case 2:
-                BorrowreCordsCordsMain bcm = new BorrowreCordsCordsMain();
-                bcm.BorrowCaords();
-                break;
-            case 3:
-                FinesMain fm = new FinesMain();
-                fm.Fines();
-                break;
-            case 4:
-                ReadersMain rm = new ReadersMain();
-                rm.operate();
-                break;
-            case 5:
-                ReservationRecordsMain rrm = new ReservationRecordsMain();
-                rrm.RRmain();
-                break;
-            case 0:
-                StartView.mainView();
-                break;
-            default:
-                System.out.println("请重新选择");
-                StartView.mainView();
-                break;
+                    }
+                    break;
+                case 2:
+                    BorrowreCordsCordsMain bcm = new BorrowreCordsCordsMain();
+                    bcm.BorrowCaords();
+                    break;
+                case 3:
+                    FinesMain fm = new FinesMain();
+                    fm.Fines();
+                    break;
+                case 4:
+                    ReadersMain rm = new ReadersMain();
+                    rm.operate();
+                    break;
+                case 5:
+                    ReservationRecordsMain rrm = new ReservationRecordsMain();
+                    rrm.RRmain();
+                    break;
+                case 0:
+                    StartView.mainView();
+                    break;
+                default:
+                    System.out.println("请重新选择");
+                    StartView.mainView();
+                    break;
+
+            }
 
         }
-
     }
 }
